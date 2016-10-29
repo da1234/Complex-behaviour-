da4618f73e0d8d185a8d5858b239e9889caf945c
da1234 <darrell.adjei@hotmail.com>
2016-10-29 15:49:56 +0100

    Add files via upload

--- RPSboard.py
+++ RPSboard.py
@@ -11,32 +11,134 @@ import RSPmodel as m
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
-BLUE = (0, 255, 0)
+BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 
  
 # This sets the WIDTH and HEIGHT of each grid location
-WIDTH = 20
-HEIGHT = 20
+WIDTH = 5
+HEIGHT = 5
  
 # This sets the margin between each cell
 MARGIN = 5
 
 #initialising the model 
-model = m.RSPmodel()
+model = m.model(3)
  
 # initialising Board 
 grid = []
-grd_sz = 10
-
-for row in range(grd_sz):
+grd_sz = 80
+      
+for row in range(grd_sz+1):
     # Add an empty array that will hold each cell
     # in this row
     grid.append([])
-    for column in range(grd_sz):
-        #grid[row].append(0)  # Append a cell
-        player = pl.Player(rndm.randint(1,3))
+    for column in range(grd_sz+1):
+        player = pl.Player(0)
         grid[row].append(player)
+        
+        
+## trying to segment the page    
+for i in range(26):
+    for j in range(26):
+    
+        grid[i][j].set_old_strat(1)
+        grid[i][j].set_new_strat(1)
+
+for i in range(26,51,1):
+    for j in range(26):
+    
+        grid[i][j].set_old_strat(2)
+        grid[i][j].set_new_strat(2)
+
+for i in range(51,81,1):   
+    for j in range(21):
+    
+        grid[i][j].set_old_strat(3)
+        grid[i][j].set_new_strat(3)
+ ####       
+        
+for i in range(26):
+    for j in range(26,51,1):
+    
+        grid[i][j].set_old_strat(3)
+        grid[i][j].set_new_strat(3)
+
+for i in range(26,51,1):
+    for j in range(26,51,1):
+    
+        grid[i][j].set_old_strat(1)
+        grid[i][j].set_new_strat(1)
+
+for i in range(51,81,1):   
+    for j in range(25,51,1):
+    
+        grid[i][j].set_old_strat(2)
+        grid[i][j].set_new_strat(2)
+##
+
+for i in range(26):
+    for j in range(51,81,1):
+    
+        grid[i][j].set_old_strat(2)
+        grid[i][j].set_new_strat(2)
+
+for i in range(26,51,1):
+    for j in range(51,81,1):
+    
+        grid[i][j].set_old_strat(3)
+        grid[i][j].set_new_strat(3)
+
+for i in range(51,81,1):   
+    for j in range(51,81,1):
+    
+        grid[i][j].set_old_strat(1)
+        grid[i][j].set_new_strat(1)
+        
+
+
+##starting with 1 pixel of two 
+#for row in range(grd_sz+1):
+#    # Add an empty array that will hold each cell
+#    # in this row
+#    grid.append([])
+#    for column in range(grd_sz+1):
+#        player = pl.Player(1)
+#        grid[row].append(player)
+#
+#
+#grid[40][20].set_old_strat(3)
+#grid[40][20].set_new_strat(3)
+#
+#grid[40][60].set_old_strat(2)
+#grid[40][60].set_new_strat(2)
+
+    
+    
+## this gave random results      
+#for i in range(6):
+#    
+#    strat = rndm.randint(1,3) 
+#    
+#    grid[rndm.randint(1,159)][rndm.randint(1,159)].set_old_strat(strat)
+#    grid[rndm.randint(1,159)][rndm.randint(1,159)].set_new_strat(strat)
+    
+      
+
+ 
+        
+#this was the original 
+#        
+#for row in range(grd_sz):
+#    # Add an empty array that will hold each cell
+#    # in this row
+#    
+#    grid.append([])
+#    
+#    for column in range(grd_sz):
+#        #grid[row].append(0)  # Append a cell
+#        player = pl.Player(rndm.randint(1,3))
+#        grid[row].append(player)
  
 
  
@@ -45,7 +147,7 @@ for row in range(grd_sz):
 pygame.init()
  
 # Set the HEIGHT and WIDTH of the screen
-WINDOW_SIZE = [255, 255]
+WINDOW_SIZE = [800,800]#[255, 255]
 screen = pygame.display.set_mode(WINDOW_SIZE)
  
 # Set title of screen
@@ -62,48 +164,48 @@ while not done:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # User clicks the mouse. Get the position
-            pos = pygame.mouse.get_pos()
-            # Change the x/y screen coordinates to grid coordinates
-            column = pos[0] // (WIDTH + MARGIN)
-            row = pos[1] // (HEIGHT + MARGIN)
-            # Set that location to one
-            grid[row][column] = 1
-            print("Click ", pos, "Grid coordinates: ", row, column)
+        
             
-        else:
-            
-            #for each player get his neighbours coords and play 
-            
-            for row_pos,row_values in enumerate(grid):
-                for col_pos,col_val in enumerate(row_values):
-                    
-                    plyr_coords = [row_pos+1,col_pos+1]
-                    n_coords = model.get_player_n(plyr_coords,grd_sz)
-                    n_values = model.get_n_values(n_coords, grid)
-                    model.play(grid[row_pos][col_pos],n_values)
-                    
-                
-    
-                
+    #else
+        #elif event.type == pygame.MOUSEBUTTONDOWN:
+        #     # User clicks the mouse. Get the position
+        #    pos = pygame.mouse.get_pos()
+        #    # Change the x/y screen coordinates to grid coordinates
+        #    column = pos[0] // (WIDTH + MARGIN)
+        #    row = pos[1] // (HEIGHT + MARGIN)
+        #    # Set that location to one
+        #    grid[row][column].set_new_strat(1) 
             
             
+            
+            
+    #for each player get his neighbours coords and play  
+            
+    for row_pos,row_values in enumerate(grid):
+        for col_pos,col_val in enumerate(row_values):
+                        
+                    #print row_pos+1,col_pos+1
+                        
+            plyr_coords = [row_pos+1,col_pos+1]
+            n_coords = model.get_player_n(plyr_coords,grd_sz)
+            n_values = model.get_n_values(n_coords, grid)
+            model.play(grid[row_pos][col_pos],n_values)
+            model.change_strat(grid[row_pos][col_pos],n_values)
+                          
  
     # Set the screen background
     screen.fill(BLACK)
- 
     # Draw the grid
-    for row in range(10):
-        for column in range(10):
-            color = WHITE
-            if grid[row][column].get_strat() == 1:
+    for row in range(grd_sz):
+        for column in range(grd_sz):
+            color = BLACK
+            if grid[row][column].get_old_strat() == 1:
                 color = GREEN
                 
-            elif grid[row][column].get_strat() == 2:
+            elif grid[row][column].get_old_strat() == 2:
                 color = BLUE
                 
-            elif grid[row][column].get_strat() == 3:
+            elif grid[row][column].get_old_strat() == 3:
                 color = RED
                 
             pygame.draw.rect(screen,
--- RSPmodel.py
+++ RSPmodel.py
@@ -1,15 +1,11 @@
-
-import player as pl 
-import numpy as np 
-import RPSboard 
+import math 
+import random as rndm
 
   
-class RSPmodel:
+class model(object):
     
-    def __init__(self):
-           self.R = 1
-           self.S = 2
-           self.P = 3 
+    def __init__(self, no_strats):
+           self.no_strats = no_strats
            
         
         
@@ -26,15 +22,22 @@ class RSPmodel:
         N_coords.append([p_coords[0]+1,p_coords[1]-1])
         N_coords.append([p_coords[0]+1,p_coords[1]+1])
         
-        for n_coords in N_coords: 
-            for pos in n_coords: 
-                if pos <=grd_sz and pos >=1:
+       # print N_coords
+        for n_coord, n_vals in enumerate(N_coords):
+           for i,j in enumerate(n_vals):
+               
+               if j <=grd_sz and j >=1:
                     
-                    pass
+                    pass 
                 
-                else: 
-                    pos = abs(pos-grd_sz)
-                    
+               else: 
+                   # print j
+                    j = abs(j-grd_sz)
+                    N_coords[n_coord][i] = j
+                    #print "this is afterwards",j 
+               
+        
+        #print N_coords           
         return N_coords 
     
     def get_n_values(self, n_coords, grid):
@@ -42,6 +45,7 @@ class RSPmodel:
         N_values = []
         
         for cords in n_coords:
+           # print cords
             N_values.append(grid[cords[0]-1][cords[1]-1])
         
         return N_values  
@@ -50,10 +54,167 @@ class RSPmodel:
         
         
         
+    #  this is for the original   
+    #def play(self,player,neighbours):
+    #    
+    #    n_strats = []
+    #    p_strat = player.get_strat()
+    #    
+    #    
+    #    
+    #    for neighbour in neighbours:
+    #        
+    #        n_strats.append(neighbour.get_strat())
+    #        
+    #    #print n_strats
+    #    
+    #    for n_strat in n_strats:
+    #        
+    #        current_score = player.get_score()
+    #        
+    #        if p_strat == 1 and n_strat == 2:
+    #            
+    #            player.set_score(current_score -1)
+    #        
+    #        elif p_strat == 1 and n_strat == 3:
+    #            
+    #             player.set_score(current_score +1)
+    #        
+    #        elif p_strat == 2 and n_strat == 1:
+    #            
+    #             player.set_score(current_score +1)
+    #             
+    #        elif p_strat == 2 and n_strat == 3:
+    #            
+    #             player.set_score(current_score -1)
+    #             
+    #        elif p_strat == 3 and n_strat == 1:
+    #            
+    #             player.set_score(current_score -1)
+    #             
+    #        elif p_strat == 3 and n_strat == 2:
+    #            
+    #             player.set_score(current_score +1)
+    #             
+    #        else: 
+    #            pass 
+                 
+           # print player.get_score()
+           
+      
         
     def play(self,player,neighbours):
         
-        p_strat = player.get_strat
+            n_strats = []
+            player.set_old_strat(player.get_new_strat())
+            p_strat = player.get_old_strat()
+    
+            for neighbour in neighbours:
+                
+                n_strats.append(neighbour.get_old_strat())
+            
+            for n_strat in n_strats:
+                
+                current_score = player.get_score()
+                
+                if p_strat == 1 and n_strat == 2:            
+     
+                    player.set_score(current_score -1)
+            
+                elif p_strat == 2 and n_strat == 3:
+                
+                    player.set_score(current_score -1)
+            
+                elif p_strat == 3 and n_strat == 1:
+                
+                    player.set_score(current_score -1)
+                    
+                else: 
+                    pass 
+                    
+                
+                    
+                 
+    # this is for the original             
+    #def change_strat(self, player,neighbours):
+    #    
+    #    cnt = 0
+    #    
+    #    n_strats = []
+    #    p_score = player.get_score()
+    #    
+    #    for neighbour in neighbours:
+    #        
+    #        n_strats.append(neighbour.get_strat())
+    #    
+    #    
+    #
+    #    
+    #    if p_score <= 0:
+    #        
+    #        n_av = int(math.ceil(sum(n_strats)/ len(n_strats)))
+    #        print n_av
+    #        
+    #        player.set_strat(n_av)
+    #        player.set_score(0)
+            
+            
+                 
+    def change_strat(self, player,neighbours):
+        
+        
+        n_strats = []
+        p_score = player.get_score()
+        
+        for neighbour in neighbours:
+            
+            if neighbour.isColoured() == True:
+                  
+                n_strats.append(neighbour.get_old_strat())
+                
+        
+        if p_score < 0:
+            
+            
+            if player.get_old_strat() == 1:
+                
+                player.set_new_strat(2)
+                
+            
+            elif player.get_old_strat() == 2:
+                
+                player.set_new_strat(3)
+            
+            elif player.get_old_strat() == 3:
+                
+                player.set_new_strat(1)
+            
+            else:
+                
+                pass 
+        
+        elif player.get_old_strat() == 0 and len(n_strats)>0:
+        
+            
+            player.set_new_strat(int(math.ceil(sum(n_strats)/ len(n_strats))))
+            
+            
+                            
+        
+        player.set_score(0)
+            
+            
+        
+        
+        
+
+                
+                
+    
+    
+            
+        
+            
         
         
         
