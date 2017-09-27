1364f869200b66e3eaaa944a898c264f171a1242
da1234 <darrell.adjei@hotmail.com>
2017-09-27 23:53:20 +0100

    Update Vertex.py

--- Vertex.py
+++ Vertex.py
@@ -6,22 +6,17 @@ class Vertex:
             self.id = key
             self.connectedTo = {}
         
-        def  addNbr(self,nbr, weight=0):
-            
+        def  addNbr(self,nbr, weight=0):            
             self.connectedTo[nbr]  = weight
             
-        def __str__(self):
-            
+        def __str__(self):            
             return str(self.id) + "connected to:" + str ([x.id for x in self.connectedTo])
             
-        def getConnections(self):
-            
+        def getConnections(self):            
             return self.connectedTo.keys()
             
-        def getId(self):
-            
+        def getId(self):            
             return self.id
             
-        def getWeight(self,nbr):
-            
+        def getWeight(self,nbr):            
             return self.connectedTo[nbr]
