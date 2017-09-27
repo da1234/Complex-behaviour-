170670ddb81a418ec62afc9a8232b7e12f2f0dbc
da1234 <darrell.adjei@hotmail.com>
2017-09-27 23:51:06 +0100

    Update RSPmodel.py

--- RSPmodel.py
+++ RSPmodel.py
@@ -10,13 +10,9 @@ class RSPmodel:
            self.R = 1
            self.S = 2
            self.P = 3 
-           
-        
-        
-    def get_player_n(self,p_coords, grd_sz):
-        
-        N_coords = []
-        
+                           
+    def get_player_n(self,p_coords, grd_sz):        
+        N_coords = []        
         N_coords.append([p_coords[0]+1,p_coords[1]])
         N_coords.append([p_coords[0]-1,p_coords[1]])
         N_coords.append([p_coords[0]-1,p_coords[1]-1])
@@ -24,64 +20,23 @@ class RSPmodel:
         N_coords.append([p_coords[0],p_coords[1]-1])
         N_coords.append([p_coords[0]-1,p_coords[1]+1])
         N_coords.append([p_coords[0]+1,p_coords[1]-1])
-        N_coords.append([p_coords[0]+1,p_coords[1]+1])
-        
+        N_coords.append([p_coords[0]+1,p_coords[1]+1])        
         for n_coords in N_coords: 
             for pos in n_coords: 
-                if pos <=grd_sz and pos >=1:
-                    
-                    pass
-                
+                if pos <=grd_sz and pos >=1:                    
+                    pass                
                 else: 
-                    pos = abs(pos-grd_sz)
-                    
+                    pos = abs(pos-grd_sz)                    
         return N_coords 
     
-    def get_n_values(self, n_coords, grid):
-        
-        N_values = []
-        
+    def get_n_values(self, n_coords, grid):        
+        N_values = []        
         for cords in n_coords:
-            N_values.append(grid[cords[0]-1][cords[1]-1])
-        
+            N_values.append(grid[cords[0]-1][cords[1]-1])        
         return N_values  
-        
-        
-        
-        
-        
-        
-    def play(self,player,neighbours):
-        
+     
+    def play(self,player,neighbours):        
         p_strat = player.get_strat
         
         
-        
-        
-        
-        
-        
-        
-        
-        
           
-        
-    
-                    
-                    
-                    
-        
-        
-        
-        
-        
-                
-                
-                
-                 
-                    
-                
-        
-        
-        
-              
