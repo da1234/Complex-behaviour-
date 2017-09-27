cac9493d1c2ee5412d312f30f780077fcbfdb540
da1234 <darrell.adjei@hotmail.com>
2017-09-27 23:55:11 +0100

    Update player.py

--- player.py
+++ player.py
@@ -3,38 +3,20 @@ class Player:
     
     
       
-    def __init__(self, strategy):
-        
+    def __init__(self, strategy):        
         self.strategy = strategy
-        self.score = 0 
+        self.score = 0         
         
-        
-    def set_strat(self, n_strat):
-        
+    def set_strat(self, n_strat):        
         self.strategy = n_strat
         
-    def get_strat(self):
-        
+    def get_strat(self):        
         return self.strategy 
         
     def get_score(self):
         return self.score
         
-    def set_score(self, n_score):
-        
+    def set_score(self, n_score):        
         self.score = n_score 
-        
-        
-        
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
         
