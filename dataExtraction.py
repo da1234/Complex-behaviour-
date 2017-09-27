8987964c7dc786bf5a747a129b99f52eff0b4177
da1234 <darrell.adjei@hotmail.com>
2017-09-27 23:53:49 +0100

    Update dataExtraction.py

--- dataExtraction.py
+++ dataExtraction.py
@@ -5,22 +5,15 @@ import numpy as np
 def extract():
 
         for size in range(1,18,1):
-
           prob_array = np.zeros(20)
-
-          cnt = 0
-        
-          with open('/Users/darrelladjei/python/bsc/mesaStuff/SystemSizeData/'+str(size)+'.txt','r') as f: 
-        
-
+          cnt = 0        
+          with open('/Users/darrelladjei/python/bsc/mesaStuff/SystemSizeData/'+str(size)+'.txt','r') as f:         
             while cnt < 20:
                 prob = float(f.readline())
                 prob_array[cnt] = prob
                 cnt+=1
-
           mean = np.mean(prob_array)
           std = np.std(prob_array)
           both = np.array([mean,std])
-          np.savetxt('/Users/darrelladjei/python/bsc/mesaStuff/SystemSizePlottingData/' + str(size)+'.txt',both)
-                
+          np.savetxt('/Users/darrelladjei/python/bsc/mesaStuff/SystemSizePlottingData/' + str(size)+'.txt',both)                
 extract()
